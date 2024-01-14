fae692305b769f6087075d5c47fcad285053d60e
E-jiwoo <livei07@naver.com>
2024-01-14 23:30:15 +0900

    CHORE :: reset - 파일 정리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import Page from "./component/index";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./component/Main/index";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
   return (
     <div>
-      <Page />
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Main />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+}
+`;
 
 export default App;
