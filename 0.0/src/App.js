c5127fdf30273d1d089eea3e26fa09c557fd67f1
E-jiwoo <livei07@naver.com>
2024-01-23 23:49:22 +0900

    Add :: GlobalStyles - padding과 margin 0으로 세팅

--- 0.0/src/App.js
+++ 0.0/src/App.js
@@ -1,11 +1,20 @@
 import Main from "./pages/Main/index";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyles />
       <Main />
-    </div>
+    </>
   );
 }
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0; 
+  }
+`;
 
 export default App;
