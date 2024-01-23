d789529ad2e9966b4414f6d4c6766d13a60172dd
E-jiwoo <livei07@naver.com>
2024-01-23 23:49:50 +0900

    Add :: background color - 배경색 검은색으로 변경

--- 0.0/src/pages/Main/index.js
+++ 0.0/src/pages/Main/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import * as S from "../../styles/theme";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <>
+      <S.Background></S.Background>
+    </>
+  );
 };
 
 export default index;
