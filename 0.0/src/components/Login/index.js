0a8a4baaefcce56dabed6b9d56eedb21e2fb702b
huise0ng <shinhuiseong07@gmail.com>
2024-02-20 01:05:00 +0900

    ADD :: 로그인 성공 / 로그인 실패

--- 0.0/src/components/Login/index.js
+++ 0.0/src/components/Login/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { backgroundStyle, modalStyle, logoStyle, textStyle, inputStyle } from './style';
 import { doc, getDoc } from 'firebase/firestore';
 import { firestore } from '../../firebase';
@@ -7,12 +8,13 @@ import logo from "../../assets/logo.svg";
 const Search = async (str1, str2) => {
   const umbrellaRef = doc(firestore, "ID", str1);
   const umbrellaDoc = await getDoc(umbrellaRef);
-  return umbrellaDoc.data()?.Password === str2; // 비교할 값이 데이터의 어떤 부분인지에 따라 수정해야 합니다.
+  return umbrellaDoc.data()?.Password === str2;
 };
 
 const LoginModal = () => {
   const [inputValue1, setInputValue1] = useState('');
   const [inputValue2, setInputValue2] = useState('');
+  const navigate = useNavigate(); 
 
   const handleInputChange1 = (event) => {
     setInputValue1(event.target.value);
@@ -22,18 +24,20 @@ const LoginModal = () => {
     setInputValue2(event.target.value);
   };
 
-  const handleKeyDown = async (event) => {
-    if (event.key === 'Enter') {
-      const result = await Search(inputValue1, inputValue2);
-      if (result) {
-        console.log("login");
-      } else {
-        console.log("fail");
-      }
-      setInputValue1('');
-      setInputValue2('');
+const handleKeyDown = async (event) => {
+  if (event.key === 'Enter') {
+    const result = await Search(inputValue1, inputValue2);
+    if (result) {
+      console.log("login");
+      navigate('/'); 
+    } else {
+      console.log("fail");
+      alert("틀렸습니다 다시하세요!"); 
     }
-  };
+    setInputValue1('');
+    setInputValue2('');
+  }
+};
 
   return (
       <div style={backgroundStyle}>
@@ -49,11 +53,11 @@ const LoginModal = () => {
           />
           <input
               style={inputStyle}
-              type="text"
+              type="password" 
               placeholder="비밀번호"
               value={inputValue2}
               onChange={handleInputChange2}
-              onKeyDown={handleKeyDown} // Enter 키 입력 시 검색
+              onKeyDown={handleKeyDown}
           />
         </div>
       </div>
