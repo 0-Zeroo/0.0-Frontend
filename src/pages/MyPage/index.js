327e5af75a486d5c756e66b5057c2bdc16d163a6
huise0ng <shinhuiseong07@gmail.com>
2024-02-27 19:27:02 +0900

    FIX :: 이메일 주인 이름 뜨도록 수정

--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -1,19 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { auth } from "../../firebase"; 
+import { getAuth, onAuthStateChanged } from "firebase/auth"; 
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import * as S from "./style";
 import DateBox from "./DateBox/index";
 import Borrow from "./Borrow/index";
 
+const auth = getAuth(); 
+const db = getFirestore(); 
+
 const Index = () => {
-  const [userEmail, setUserEmail] = useState(null); 
+  const [userName, setUserName] = useState(""); 
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        setUserEmail(user.email); 
+        const userRef = doc(db, "People", user.email); 
+        getDoc(userRef).then((docSnap) => {
+          if (docSnap.exists()) {
+            setUserName(docSnap.data().NAME); 
+          } else {
+            setUserName("이름 없음");
+          }
+        }).catch(error => {
+          console.error("Error fetching user data:", error);
+        });
       } else {
-        setUserEmail(null); 
+        setUserName(""); 
       }
     });
 
@@ -24,7 +37,7 @@ const Index = () => {
     <>
       <S.Background>
         <S.Stroke />
-        <S.Info>{userEmail ? userEmail : "로그인 하세요"}</S.Info>
+        <S.Info>{userName ? userName : "로그인 하세요"}</S.Info>
         <Link to="/password">
           <S.Password>비밀번호 바꾸기</S.Password>
         </Link>
