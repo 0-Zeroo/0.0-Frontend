dc5c83a40426143762c2218b00720d51ac6570bf
dongwookkim3 <dongwook.3.kim@gmail.com>
2024-03-24 01:43:57 +0900

    ADD : 우산 대여 횟수 & 현재 우산 대여 DB 연결
    추가적인 구현 필요, 내일 구현함

--- src/components/Header/HeaderS/index.js
+++ src/components/Header/HeaderS/index.js
@@ -4,59 +4,61 @@ import * as S from './style';
 import logo from '../../../assets/logo.svg';
 import user from '../../../assets/user.svg';
 import { auth } from '../../../firebase';
-import { getFirestore, doc, getDoc } from 'firebase/firestore'; 
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const Header = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true');
   const [userName, setUserName] = useState('');
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         setIsLoggedIn(true);
-        const firestore = getFirestore(); 
-        const userRef = doc(firestore, 'People', userAuth.email); 
+        localStorage.setItem('isLoggedIn', 'true'); // 로그인 상태를 로컬 스토리지에 저장
+        const firestore = getFirestore();
+        const userRef = doc(firestore, 'People', userAuth.email);
         const docSnap = await getDoc(userRef);
 
         if (docSnap.exists()) {
-          setUserName(docSnap.data().NAME); 
+          setUserName(docSnap.data()?.Name);
         } else {
           console.log("No such document!");
           setUserName('');
         }
       } else {
         setIsLoggedIn(false);
+        localStorage.setItem('isLoggedIn', 'false'); // 로그아웃 상태를 로컬 스토리지에 저장
         setUserName('');
       }
     });
 
-    return () => unsubscribe(); 
+    return () => unsubscribe();
   }, []);
 
   return (
-    <>
-      <S.Container>
-        <S.ImgBox>
-          <Link to="/">
-            <S.Img src={logo} alt="logo" />
-          </Link>
-          <Link to="/mypage">
-            <S.Img src={user} alt="user" />
-          </Link>
-        </S.ImgBox>
-        <S.TextBox>
-          {isLoggedIn ? (
-            <S.Text style={{ color: 'white' }}>{userName}</S.Text>
-          ) : (
-            <Link to="/login">
-              <S.Box>
-                <S.Text>로그인</S.Text>
-              </S.Box>
+      <>
+        <S.Container>
+          <S.ImgBox>
+            <Link to="/">
+              <S.Img src={logo} alt="logo" />
             </Link>
-          )}
-        </S.TextBox>
-      </S.Container>
-    </>
+            <Link to="/mypage">
+              <S.Img src={user} alt="user" />
+            </Link>
+          </S.ImgBox>
+          <S.TextBox>
+            {isLoggedIn ? (
+                <S.Text style={{ color: 'white' }}>{userName}</S.Text>
+            ) : (
+                <Link to="/login">
+                  <S.Box>
+                    <S.Text>로그인</S.Text>
+                  </S.Box>
+                </Link>
+            )}
+          </S.TextBox>
+        </S.Container>
+      </>
   );
 };
 
--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
-import {getAuth, onAuthStateChanged} from "firebase/auth";
-import {getFirestore, doc, getDoc} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import * as S from "./style";
 
 const auth = getAuth();
@@ -9,6 +9,8 @@ const db = getFirestore();
 
 const Index = () => {
     const [userName, setUserName] = useState("");
+    const [userBorrow, setUserBorrow] = useState(0);
+    const [userSeat, setUserSeat] = useState("");
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -16,7 +18,9 @@ const Index = () => {
                 const userRef = doc(db, "People", user.email);
                 getDoc(userRef).then((docSnap) => {
                     if (docSnap.exists()) {
-                        setUserName(docSnap.data().NAME);
+                        setUserName(docSnap.data()?.Name || "");
+                        setUserBorrow(docSnap.data()?.Borrow || 0);
+                        setUserSeat(docSnap.data()?.Seat || "");
                     } else {
                         setUserName("이름 없음");
                     }
@@ -25,6 +29,8 @@ const Index = () => {
                 });
             } else {
                 setUserName("");
+                setUserBorrow(0);
+                setUserSeat("");
             }
         });
 
@@ -34,18 +40,17 @@ const Index = () => {
     return (
         <>
             <S.Background>
-                {/*<S.Stroke />*/}
                 <S.Info>{userName ? userName : "로그인 하세요"}</S.Info>
                 <Link to="/password">
                     <S.Password>비밀번호 바꾸기</S.Password>
                 </Link>
                 <S.Times>
                     우산 대여 횟수
-                    <S.Num>5</S.Num>
+                    <S.Num>{userBorrow}</S.Num>
                 </S.Times>
                 <S.State>
                     현재 우산 대여
-                    <S.Circle/>
+                    <S.Num>{userSeat === true ? 1 : 0}</S.Num>
                 </S.State>
             </S.Background>
         </>
